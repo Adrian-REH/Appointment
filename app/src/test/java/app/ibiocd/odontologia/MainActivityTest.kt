f0378c9b97ce2549a98a700cb63f06ae16a542d1
Adrian-REH <adrianherrera.r.e@gmail.com>
2022-09-18 13:57:55 -0300

    O

---
+++ app/src/test/java/app/ibiocd/odontologia/MainActivityTest.kt
@@ -0,0 +1,16 @@
+package app.ibiocd.odontologia
+
+import io.mockk.MockKAnnotations
+import io.mockk.impl.stub.StubRepository
+import org.junit.Before
+
+internal class MainActivityTest{
+
+    @Before
+    fun onBefore(){
+        MockKAnnotations.init(this)
+
+
+
+    }
+}
